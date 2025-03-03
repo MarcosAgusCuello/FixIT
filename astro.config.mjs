abf0bd8285bc4d5fffc8c659f00f2e28ef8c2ec5
Layka132- <cuellomarcos2017@gmail.com>
2025-03-03 11:04:37 -0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,25 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const SERVER_PORT = 3000;
+//the url to acces your blog during local development
+const LOCALHOST_URL = `https://localhost:${SERVER_PORT}`;
+//the url to access your blog after deploying it somewhere (eg. Netlify)
+const LIVE_URL = "https://FixIT.github.io";
+// this is the astro command your npm scripts runs
+const SCRIPT = import.meta.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+//when you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://FixIT.github.io',
+  base: '',
     vite: {
         plugins: [tailwindcss()],
       },
